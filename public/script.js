5cf929798f2b01d035146307988d57e084624db8
Jason - CRMNext <jasontmiani@gmail.com>
2023-07-28 14:43:43 -0700

    add excel doc and link to excel doc

--- public/script.js
+++ public/script.js
@@ -1,52 +1,51 @@
 document.addEventListener('DOMContentLoaded', updateNote)
-// document.addEventListener('DOMContentLoaded', displayExcel)
+document.addEventListener('DOMContentLoaded', displayExcel)
 
-const templates = {
-        'Disputed Credit Card Transaction': `
-    <h3><u>Credit Card Dispute</u></h3>
-    1. Immediately block this card to prevent any further fraudulent charges.
-    <br>
-    <br>
-    2. Before submitting the dispute, ensure the customer/member has already called the merchant to inquire about the charge.
-    <br>
-    <br>
-    3. Inform customer/member it will take up to 10 business days to receive provisional credit for this charge
-    <br>
-    <br>
-    <strong>Links</strong>
-    
-    <ul>
-      <li><a href="https://crmnext.us" target="_blank"> Policy and Procedures Manual</a></li>
-      <li><a href="https://crmnext.us" target="_blank"> Policy and Procedures Manual</a></li>
-      <li><a href="https://crmnext.us" target="_blank"> Policy and Procedures Manual</a></li>
-      <li><a href="https://crmnext.us" target="_blank"> Policy and Procedures Manual</a></li>
-    </ul>
+// const templates = {
+//         'Disputed Credit Card Transaction': `
+//     <h3><u>Credit Card Dispute</u></h3>
+//     1. Immediately block this card to prevent any further fraudulent charges.
+//     <br>
+//     <br>
+//     2. Before submitting the dispute, ensure the customer/member has already called the merchant to inquire about the charge.
+//     <br>
+//     <br>
+//     3. Inform customer/member it will take up to 10 business days to receive provisional credit for this charge
+//     <br>
+//     <br>
+//     <strong>Links</strong>
 
-    `,
-    'Disputed Debit Card Transaction': `
-    <h3><u>Debit Card Dispute</u></h3>
-    1. Immediately block this card to prevent any further fraudulent charges.
-    <br>
-      <br>
-        2. Before submitting the dispute, ensure the customer/member has already called the merchant to inquire about the charge.
-        <br>
-          <br>
-            3. Inform customer/member it will take up to 10 business days to receive provisional credit for this charge
-            <br>
-              <br>
-                <strong>Links</strong>
-                <ul>
-                  <li><a href="https://crmnext.us" target="_blank"> Policy and Procedures Manual</a></li>
-                  <li><a href="https://crmnext.us" target="_blank"> Policy and Procedures Manual</a></li>
-                  <li><a href="https://crmnext.us" target="_blank"> Policy and Procedures Manual</a></li>
-                  <li><a href="https://crmnext.us" target="_blank"> Policy and Procedures Manual</a></li>
-                </ul>
-    `,
-    'Auto Loan': 'Auto Loan Test'
-    }
+//     <ul>
+//       <li><a href="https://crmnext.us" target="_blank"> Policy and Procedures Manual</a></li>
+//       <li><a href="https://crmnext.us" target="_blank"> Policy and Procedures Manual</a></li>
+//       <li><a href="https://crmnext.us" target="_blank"> Policy and Procedures Manual</a></li>
+//       <li><a href="https://crmnext.us" target="_blank"> Policy and Procedures Manual</a></li>
+//     </ul>
 
+//     `,
+//     'Disputed Debit Card Transaction': `
+//     <h3><u>Debit Card Dispute</u></h3>
+//     1. Immediately block this card to prevent any further fraudulent charges.
+//     <br>
+//       <br>
+//         2. Before submitting the dispute, ensure the customer/member has already called the merchant to inquire about the charge.
+//         <br>
+//           <br>
+//             3. Inform customer/member it will take up to 10 business days to receive provisional credit for this charge
+//             <br>
+//               <br>
+//                 <strong>Links</strong>
+//                 <ul>
+//                   <li><a href="https://crmnext.us" target="_blank"> Policy and Procedures Manual</a></li>
+//                   <li><a href="https://crmnext.us" target="_blank"> Policy and Procedures Manual</a></li>
+//                   <li><a href="https://crmnext.us" target="_blank"> Policy and Procedures Manual</a></li>
+//                   <li><a href="https://crmnext.us" target="_blank"> Policy and Procedures Manual</a></li>
+//                 </ul>
+//     `,
+//     'Auto Loan': 'Auto Loan Test'
+//     }
 
-function updateNote() {
+async function updateNote() {
     const dmsNoteTarget = document.getElementById('dmsNoteTarget')
 
     var subject = ''
@@ -54,42 +53,36 @@ function updateNote() {
     const caseSubject = document.getElementsByName('CASE_SUBCATEGORY1')[0]
     const leadSubject = document.getElementsByName('cust_6055')[0]
 
-    if(caseSubject){
-      subject = caseSubject.value
+    if (caseSubject) {
+        subject = caseSubject.value
     }
 
     if (leadSubject) {
         subject = leadSubject.value
     }
 
+    const url = 'https://crmnextus-gold8-dms-scripts.s3.amazonaws.com/Note+Templates.xlsx'
 
-    if(templates[subject]){
-        dmsNoteTarget.innerHTML = templates[subject]
-    } else {
-        dmsNoteTarget.innerHTML = 'No Note!'
+    try {
+        const file = await (await fetch(url)).arrayBuffer()
+
+        const workbook = XLSX.read(file)
+
+        const worksheet = workbook.Sheets[workbook.SheetNames[0]]
+
+        const rawData = XLSX.utils.sheet_to_json(worksheet)
+
+        const output = rawData.filter((item) => item.Subject == subject)
+        console.log(output)
+
+        if (output) {
+            dmsNoteTarget.innerHTML = output[0].Description
+        } else {
+            dmsNoteTarget.innerHTML = 'No Note!'
+        }
+    } catch (err) {
+        console.log(err)
     }
 
-
-
-    
+    return 'Success'
 }
-
-// async function displayExcel(){
-  
-//   const url = 'https://crmnextus-gold8-dms-scripts.s3.amazonaws.com/Note+Templates.xlsx'
-  
-//   const file = await (await fetch(url)).arrayBuffer()
-
-//   const workbook = XLSX.read(file);
-
-//   const worksheet = workbook.Sheets[workbook.SheetNames[0]]
-
-//   const rawData = XLSX.utils.sheet_to_json(worksheet)
-//   console.log(rawData)
-
-//   const output = rawData.filter(item => item.Subject == 'Auto Loan')
-  
-
-//   document.getElementById('excelNoteTarget').innerHTML = output[0].Description
-
-// }
