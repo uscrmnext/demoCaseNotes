127d81dbae1c4c722d6d2042369b6245e9f6ff6c
Jason - CRMNext <jasontmiani@gmail.com>
2023-07-28 11:28:01 -0700

    update the selections

--- public/script.js
+++ public/script.js
@@ -5,14 +5,49 @@ function updateNote() {
     const caseSubject = document.getElementsByName('CASE_SUBCATEGORY1')[0].value
 
     const selections = {
-        creditCardDisute: `1. Immediately block this card to prevent any further fraudulent charges. 
+        creditCardDisute: `
+    <h3>Credit Card Dispute</h3>
+    1. Immediately block this card to prevent any further fraudulent charges.
+    <br>
+    <br>
+    2. Before submitting the dispute, ensure the customer/member has already called the merchant to inquire about the charge.
+    <br>
+    <br>
+    3. Inform customer/member it will take up to 10 business days to receive provisional credit for this charge
+    <br>
+    <br>
+    <strong>Links</strong>
+    
+    <ul>
+      <li><a href="https://crmnext.us" target="_blank"> Policy and Procedures Manual</a></li>
+      <li><a href="https://crmnext.us" target="_blank"> Policy and Procedures Manual</a></li>
+      <li><a href="https://crmnext.us" target="_blank"> Policy and Procedures Manual</a></li>
+      <li><a href="https://crmnext.us" target="_blank"> Policy and Procedures Manual</a></li>
+    </ul>
+
+    `,
+        debitCardDispute: `
+    <h3>Debit Card Dispute</h3>
+    1. Immediately block this card to prevent any further fraudulent charges.
+    < br >
+      <br>
+        2. Before submitting the dispute, ensure the customer/member has already called the merchant to inquire about the charge.
         <br>
-        <br>
-          2. Before submitting the dispute, ensure the customer/member has already called the merchant to inquire about the charge.
           <br>
+            3. Inform customer/member it will take up to 10 business days to receive provisional credit for this charge
             <br>
-        3. Inform customer / member it will take up to 10 business days to receive provisional credit for this charge`
+              <br>
+                <strong>Links</strong>
+                <ul>
+                  <li><a href="https://crmnext.us" target="_blank"> Policy and Procedures Manual</a></li>
+                  <li><a href="https://crmnext.us" target="_blank"> Policy and Procedures Manual</a></li>
+                  <li><a href="https://crmnext.us" target="_blank"> Policy and Procedures Manual</a></li>
+                  <li><a href="https://crmnext.us" target="_blank"> Policy and Procedures Manual</a></li>
+                </ul>
+
+    `
     }
+
     switch (caseSubject) {
         case 'Disputed Credit Card Transaction':
             dmsNoteTarget.innerHTML = selections.creditCardDisute
@@ -22,50 +57,5 @@ function updateNote() {
             break
     }
 
-    console.log('another update')
+    
 }
-
-
-
-    // const selections = {
-    //   creditCardDisute: `
-    // <h3>Credit Card Dispute</h3>
-    // 1. Immediately block this card to prevent any further fraudulent charges. 
-    // <br>
-    // <br>
-    // 2. Before submitting the dispute, ensure the customer/member has already called the merchant to inquire about the charge.
-    // <br>
-    // <br>
-    // 3. Inform customer/member it will take up to 10 business days to receive provisional credit for this charge
-    // <br>
-    // <br>
-    // <strong>Links</strong>
-    // <ul>
-    //   <li><a href="https://crmnext.us" target="_blank"> Policy and Procedures Manual</a></li>
-    //   <li><a href="https://crmnext.us" target="_blank"> Policy and Procedures Manual</a></li>
-    //   <li><a href="https://crmnext.us" target="_blank"> Policy and Procedures Manual</a></li>
-    //   <li><a href="https://crmnext.us" target="_blank"> Policy and Procedures Manual</a></li>
-    // </ul>
-
-    // `,
-    //   debitCardDispute: `
-    // <h3>Debit Card Dispute</h3>
-    // 1. Immediately block this card to prevent any further fraudulent charges. 
-    // < br >
-    //   <br>
-    //     2. Before submitting the dispute, ensure the customer/member has already called the merchant to inquire about the charge.
-    //     <br>
-    //       <br>
-    //         3. Inform customer/member it will take up to 10 business days to receive provisional credit for this charge
-    //         <br>
-    //           <br>
-    //             <strong>Links</strong>
-    //             <ul>
-    //               <li><a href="https://crmnext.us" target="_blank"> Policy and Procedures Manual</a></li>
-    //               <li><a href="https://crmnext.us" target="_blank"> Policy and Procedures Manual</a></li>
-    //               <li><a href="https://crmnext.us" target="_blank"> Policy and Procedures Manual</a></li>
-    //               <li><a href="https://crmnext.us" target="_blank"> Policy and Procedures Manual</a></li>
-    //             </ul>
-
-    // `
-    // }
