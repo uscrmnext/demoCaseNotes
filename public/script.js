0b593805a301af94b5ff49a6ec8aee5400a36c8f
Jason - CRMNext <jasontmiani@gmail.com>
2023-07-28 14:53:35 -0700

    remove some comments and an event listener

--- public/script.js
+++ public/script.js
@@ -1,49 +1,4 @@
 document.addEventListener('DOMContentLoaded', updateNote)
-document.addEventListener('DOMContentLoaded', displayExcel)
-
-// const templates = {
-//         'Disputed Credit Card Transaction': `
-//     <h3><u>Credit Card Dispute</u></h3>
-//     1. Immediately block this card to prevent any further fraudulent charges.
-//     <br>
-//     <br>
-//     2. Before submitting the dispute, ensure the customer/member has already called the merchant to inquire about the charge.
-//     <br>
-//     <br>
-//     3. Inform customer/member it will take up to 10 business days to receive provisional credit for this charge
-//     <br>
-//     <br>
-//     <strong>Links</strong>
-
-//     <ul>
-//       <li><a href="https://crmnext.us" target="_blank"> Policy and Procedures Manual</a></li>
-//       <li><a href="https://crmnext.us" target="_blank"> Policy and Procedures Manual</a></li>
-//       <li><a href="https://crmnext.us" target="_blank"> Policy and Procedures Manual</a></li>
-//       <li><a href="https://crmnext.us" target="_blank"> Policy and Procedures Manual</a></li>
-//     </ul>
-
-//     `,
-//     'Disputed Debit Card Transaction': `
-//     <h3><u>Debit Card Dispute</u></h3>
-//     1. Immediately block this card to prevent any further fraudulent charges.
-//     <br>
-//       <br>
-//         2. Before submitting the dispute, ensure the customer/member has already called the merchant to inquire about the charge.
-//         <br>
-//           <br>
-//             3. Inform customer/member it will take up to 10 business days to receive provisional credit for this charge
-//             <br>
-//               <br>
-//                 <strong>Links</strong>
-//                 <ul>
-//                   <li><a href="https://crmnext.us" target="_blank"> Policy and Procedures Manual</a></li>
-//                   <li><a href="https://crmnext.us" target="_blank"> Policy and Procedures Manual</a></li>
-//                   <li><a href="https://crmnext.us" target="_blank"> Policy and Procedures Manual</a></li>
-//                   <li><a href="https://crmnext.us" target="_blank"> Policy and Procedures Manual</a></li>
-//                 </ul>
-//     `,
-//     'Auto Loan': 'Auto Loan Test'
-//     }
 
 async function updateNote() {
     const dmsNoteTarget = document.getElementById('dmsNoteTarget')
